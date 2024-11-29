233af6533045b118d75ed6a602d324391eef6918
Florin Venis <florinvenis@gmail.com>
2024-11-29 15:08:18 +0200

    Finished the first task in C, started the translation into ASM

---
+++ task0.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+
+#define MEMORY_SIZE 1001
+
+int query_count = 0;
+int MEMORY[MEMORY_SIZE];
+
+void PRINT_MEMORY(){
+    int i = 0;
+    MEMORY_SCAN_LOOP:
+    if(i == 1000){
+        goto EXIT_MEMORY_SCAN_LOOP;
+    }
+
+    if(MEMORY[i] != 0){
+        int file_descriptor = MEMORY[i];
+        int file_start = i;
+        int file_end = i;
+
+        FILE_SCAN_LOOP:
+        if(MEMORY[file_end + 1] != file_descriptor){
+            goto FILE_SCAN_LOOP_EXIT;
+        }
+
+        file_end++;
+        goto FILE_SCAN_LOOP;
+
+        FILE_SCAN_LOOP_EXIT:
+        printf("%d: (%d, %d)\n", file_descriptor, file_start, file_end);
+        i = file_end;
+    }
+
+    i++;
+    goto MEMORY_SCAN_LOOP;
+
+    EXIT_MEMORY_SCAN_LOOP:
+
+}
+
+void EXEC_ADD(int file_descriptor, int file_size){
+
+    if(file_size % 8 == 0){
+        file_size = file_size / 8;
+    }
+    else{
+        file_size = file_size / 8 + 1;
+    }
+
+    int i = 0;
+    int current_chunk_start = 0;
+    int current_chunk_size = 0;
+    int current_chunk_end = 0;
+    
+    ADD_MEMORY_SCAN_LOOP:
+    if(i == MEMORY_SIZE){
+        goto ADD_MEMORY_SCAN_EXIT;
+    }
+
+    if(MEMORY[i] == 0){
+        current_chunk_size++;
+
+        if(current_chunk_size == 1){
+            current_chunk_start = i;
+        }
+
+        if(current_chunk_size == file_size){
+            current_chunk_end = current_chunk_start + current_chunk_size - 1;
+            int malloc_i = current_chunk_start;
+
+            MALLOC_LOOP:
+            if(malloc_i == current_chunk_end + 1){
+                goto ADD_MEMORY_SCAN_EXIT;
+            }
+            
+            MEMORY[malloc_i] = file_descriptor;
+            malloc_i++;
+            goto MALLOC_LOOP;
+        }
+    }
+    else{
+        current_chunk_size = 0;
+    }
+    i++;
+    goto ADD_MEMORY_SCAN_LOOP;
+
+    ADD_MEMORY_SCAN_EXIT:
+}
+
+void EXEC_GET(int file_descriptor){
+    int i = 0;
+    GET_MEMORY_SCAN_LOOP:
+    if(i == 1000){
+        goto EXIT_GET_MEMORY_SCAN_LOOP;
+    }
+
+    if(MEMORY[i] == file_descriptor){
+        int file_start = i;
+        int file_end = i + 1;
+
+        GET_FILE_SCAN_LOOP:
+        if(MEMORY[file_end + 1] != file_descriptor){
+            goto GET_FILE_SCAN_LOOP_EXIT;
+        }
+
+        file_end++;
+        goto GET_FILE_SCAN_LOOP;
+
+        GET_FILE_SCAN_LOOP_EXIT:
+        printf("(%d, %d)\n", file_start, file_end);
+        goto EXIT_GET_MEMORY_SCAN_LOOP;
+    }
+
+    i++;
+    goto GET_MEMORY_SCAN_LOOP;
+
+    EXIT_GET_MEMORY_SCAN_LOOP:
+}
+
+void EXEC_DELETE(int file_descriptor){
+    int i = 0;
+
+    DELETE_MEMORY_SCAN_LOOP:
+    if(i == 1000){
+        goto EXIT_DELETE_MEMORY_SCAN_LOOP;
+    }
+
+    if(MEMORY[i] == file_descriptor){
+        int file_it = i;
+
+        DELETE_FILE_SCAN_LOOP:
+        if(MEMORY[file_it] != file_descriptor){
+            goto EXIT_DELETE_MEMORY_SCAN_LOOP;
+        }
+        MEMORY[file_it] = 0;
+
+        file_it++;
+        goto DELETE_FILE_SCAN_LOOP;
+    }
+
+    i++;
+    goto DELETE_MEMORY_SCAN_LOOP;
+
+    EXIT_DELETE_MEMORY_SCAN_LOOP:
+}
+
+void EXEC_DEFRAGMENTATION(){
+    int i = 0;
+    int free_chunk_size = 0;
+
+    DEFRAG_MEMORY_SCAN_LOOP:
+    if(i == 1000){
+        goto EXIT_DEFRAG_MEMORY_SCAN_LOOP;
+    }
+
+    if(MEMORY[i] == 0){
+        free_chunk_size++;
+    }
+
+    if(MEMORY[i] != 0 && free_chunk_size > 0){
+        int file_descriptor = MEMORY[i];
+        int file_start = i;
+        int file_end = i + 1;
+
+        DEFRAG_FILE_SCAN_LOOP:
+        if(MEMORY[file_end] != file_descriptor){
+            goto DEFRAG_FILE_SCAN_LOOP_EXIT;
+        }
+
+        file_end++;
+        goto DEFRAG_FILE_SCAN_LOOP;
+
+        DEFRAG_FILE_SCAN_LOOP_EXIT:
+        int file_size = (file_end - file_start) * 8;
+
+        EXEC_DELETE(file_descriptor);
+        EXEC_ADD(file_descriptor, file_size);
+
+        i = file_end - free_chunk_size;
+        free_chunk_size = 0;
+    }
+
+    i++;
+    goto DEFRAG_MEMORY_SCAN_LOOP;
+
+    EXIT_DEFRAG_MEMORY_SCAN_LOOP:
+}
+
+int main(){
+    scanf("%d", &query_count);
+
+    QUERY_LOOP:
+    if(query_count == 0){
+        goto END_PROGRAM;
+    }
+    /* 
+    QUERY INSTRUCTION CODES
+    1 - ADD 
+    2 - GET
+    3 - DELETE
+    4 - DEFRAGMENTATION
+    */
+
+    int query_code = 0;
+    scanf("%d", &query_code);
+
+    if(query_code == 1){
+        int file_count = 0;
+        scanf("%d", &file_count);
+
+        ADD_LOOP:
+        if(file_count == 0){
+            goto EXIT_ADD_LOOP;
+        }
+        int file_descriptor = 0;
+        scanf("%d", &file_descriptor);
+
+        int file_size = 0;
+        scanf("%d", &file_size);
+        
+        EXEC_ADD(file_descriptor, file_size);
+
+        file_count--;
+        goto ADD_LOOP;
+
+        EXIT_ADD_LOOP:
+        PRINT_MEMORY();
+    }
+    else if(query_code == 2){
+        int file_descriptor = 0;
+        scanf("%d", &file_descriptor);
+        EXEC_GET(file_descriptor);
+    }
+    else if(query_code == 3){
+        int file_descriptor = 0;
+        scanf("%d", &file_descriptor);
+        EXEC_DELETE(file_descriptor);
+        PRINT_MEMORY();
+    }
+    else if(query_code == 4){
+        EXEC_DEFRAGMENTATION();
+        PRINT_MEMORY();
+    }
+
+    query_count--;
+    goto QUERY_LOOP;
+
+    END_PROGRAM:
+    return 0;
+}
---
+++ task0.s
@@ -0,0 +1,116 @@
+.data
+scanf_fmt: .asciz "%d"
+query_count: .space 4
+query_code: .space 4
+file_count: .space 4
+
+file_descriptor: .space 4
+file_size: .space 4
+file_start: .space 4
+file_end: .space 4
+
+.text
+EXEC_ADD:
+    ret
+
+EXEC_GET:
+    ret
+
+EXEC_DELETE:
+    ret
+
+EXEC_DEFRAGMENTATION:
+    ret
+
+.global main
+main:
+    // scanf("%d", &query_count);
+    pushl $query_count
+    pushl $scanf_fmt
+    call scanf
+    popl %ebx
+    popl %ebx
+
+QUERY_LOOP:
+    cmp $0, query_count
+    je END_PROGRAM 
+
+    // scanf("%d", &query_code);
+    pushl $query_code
+    pushl $scanf_fmt
+    call scanf
+    popl %ebx
+    popl %ebx
+
+    // QUERY INSTRUCTION CODES
+    // 1 - ADD 
+    // 2 - GET
+    // 3 - DELETE
+    // 4 - DEFRAGMENTATION
+
+    cmp $1, query_code
+    je HANDLE_ADD
+
+    cmp $2, query_code
+    je HANDLE_GET
+
+    cmp $3, query_code
+    je HANDLE_DELETE
+
+    cmp $4, query_code
+    je HANDLE_DEFRAGMENTATION
+
+HANDLE_ADD:
+    // scanf("%d", &file_count);
+    pushl $file_count
+    pushl $scanf_fmt
+    call scanf
+    popl %ebx
+    popl %ebx
+
+    ADD_LOOP:
+        cmp $0, file_count
+        je EXIT_ADD_LOOP
+
+        // scanf("%d", &file_descriptor);
+        pushl $file_descriptor
+        pushl $scanf_fmt
+        call scanf
+        popl %ebx
+        popl %ebx
+
+        // scanf("%d", &file_size);
+        pushl $file_size
+        pushl $scanf_fmt
+        call scanf
+        popl %ebx
+        popl %ebx
+
+        // EXEC_ADD(file_descriptor, file_size)
+        call EXEC_ADD
+
+        dec file_count
+        jmp ADD_LOOP
+    EXIT_ADD_LOOP:
+    jmp CONTINUE_QUERY_LOOP
+
+HANDLE_GET:
+
+    jmp CONTINUE_QUERY_LOOP
+
+HANDLE_DELETE:
+
+    jmp CONTINUE_QUERY_LOOP
+
+HANDLE_DEFRAGMENTATION:
+
+    jmp CONTINUE_QUERY_LOOP
+
+CONTINUE_QUERY_LOOP:
+    dec query_count
+    jmp QUERY_LOOP
+
+END_PROGRAM:
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
