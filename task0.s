3d6111cf9ba1828f778b424dac5705e319bd651e
Florin Venis <florinvenis@gmail.com>
2024-12-03 17:33:09 +0200

    Finished task0

--- task0.s
+++ task0.s
@@ -16,6 +16,8 @@ current_chunk_start: .long 0
 current_chunk_end: .long 0
 current_chunk_size: .long 0
 
+free_chunk_size: .long 0
+
 malloc_i: .long 0
 
 MEMORY: .space 4004
@@ -28,7 +30,7 @@ CLEAR_MEMORY:
     cmp $1001, %ecx
     je CLEAR_MEMORY_EXIT
 
-    mov $0, (%edi, %ecx, 4)
+    movl $0, (%edi, %ecx, 4)
 
     inc %ecx
     jmp CLEAR_MEMORY_LOOP
@@ -59,7 +61,7 @@ PRINT_MEMORY:
     cmp %edx, file_descriptor
     jne FILE_SCAN_LOOP_EXIT
 
-    inc file_end
+    incl file_end
     jmp FILE_SCAN_LOOP
 
     FILE_SCAN_LOOP_EXIT:
@@ -97,7 +99,7 @@ EXEC_ADD:
     cmp $0, %edx
     je ADD_INIT_MEMORY_SCAN
 
-    inc file_size
+    incl file_size
 
     ADD_INIT_MEMORY_SCAN:
     movl $0, %ecx
@@ -120,7 +122,7 @@ EXEC_ADD:
     jmp ADD_MEMORY_SCAN_FULL_CHUNK
 
     ADD_MEMORY_SCAN_EMPTY_CHUNK:
-    inc current_chunk_size
+    incl current_chunk_size
 
     // if current_chunk_size == 1
     //      current_chunk_start = i;
@@ -154,7 +156,7 @@ EXEC_ADD:
     // MEMORY[malloc_i] = file_descriptor
     mov file_descriptor, %eax
     movl %eax, (%edi, %edx, 4)
-    inc malloc_i
+    incl malloc_i
     jmp MALLOC_LOOP
 
     jmp ADD_CONTINUE_MEMORY_SCAN
@@ -184,7 +186,7 @@ EXEC_GET:
 
     movl %ecx, file_start
     movl %ecx, file_end
-    inc file_end
+    incl file_end
 
     GET_FILE_LOOP:
     // %edx = MEMORY[file_end + 1]
@@ -194,7 +196,7 @@ EXEC_GET:
     cmp %edx, file_descriptor
     jne EXIT_GET_FILE_LOOP
 
-    inc file_end
+    incl file_end
     jmp GET_FILE_LOOP
 
     EXIT_GET_FILE_LOOP:
@@ -216,9 +218,96 @@ EXEC_GET:
     ret
 
 EXEC_DELETE:
+    mov $0, %ecx
+
+    DELETE_LOOP:
+    cmp $1000, %ecx
+    je EXIT_DELETE_LOOP
+
+    // %edx = MEMORY[i]
+    movl (%edi, %ecx, 4), %edx
+    cmp file_descriptor, %edx
+    jne CONTINUE_DELETE_LOOP
+
+    movl %ecx, %eax
+
+    DELETE_FILE_SCAN_LOOP:
+    // with %eax starting from the first occurence
+    // of the file_descriptor
+    // if(MEMORY[%eax] != file_descriptor){
+    //     goto EXIT_DELETE_MEMORY_SCAN_LOOP;
+    // }
+    movl (%edi, %eax, 4), %edx
+    cmp file_descriptor, %edx
+    jne EXIT_DELETE_LOOP
+
+    movl $0, (%edi, %eax, 4)
+
+    inc %eax
+    jmp DELETE_FILE_SCAN_LOOP
+
+    CONTINUE_DELETE_LOOP:
+    inc %ecx
+    jmp DELETE_LOOP
+
+    EXIT_DELETE_LOOP:
     ret
 
 EXEC_DEFRAGMENTATION:
+    movl $0, %ecx
+    movl $0, free_chunk_size
+
+    DEFRAG_LOOP:
+    cmp $1000, %ecx
+    je EXIT_DEFRAG_LOOP
+
+    // %edx = MEMORY[i]
+    movl (%edi, %ecx, 4), %edx
+
+    cmp $0, %edx
+    jne DEFRAG_TRY_SHIFT
+
+    incl free_chunk_size
+    jmp CONTINUE_DEFRAG_LOOP
+
+    DEFRAG_TRY_SHIFT:
+    cmpl $0, free_chunk_size
+    jl CONTINUE_DEFRAG_LOOP
+
+    movl %edx, file_descriptor
+    movl %ecx, file_start
+    movl %ecx, file_end
+
+    DEFRAG_FILE_SCAN_LOOP:
+    movl file_end, %eax
+    movl (%edi, %eax, 4), %edx
+    cmp file_descriptor, %edx
+    jne EXIT_DEFRAG_FILE_SCAN_LOOP
+
+    incl file_end
+    jmp DEFRAG_FILE_SCAN_LOOP
+
+    EXIT_DEFRAG_FILE_SCAN_LOOP:
+    movl file_end, %eax
+    subl file_start, %eax
+    movl $0, %edx
+    movl $8, %ebx
+    mul %ebx
+
+    movl %eax, file_size
+    call EXEC_DELETE
+    call EXEC_ADD
+
+    movl file_end, %eax
+    subl free_chunk_size, %eax
+    movl %eax, %ecx
+    movl $0, free_chunk_size
+
+    CONTINUE_DEFRAG_LOOP:
+    inc %ecx
+    jmp DEFRAG_LOOP
+
+    EXIT_DEFRAG_LOOP:
     ret
 
 .global main
@@ -234,7 +323,7 @@ main:
     popl %ebx
 
     QUERY_LOOP:
-    cmp $0, query_count
+    cmpl $0, query_count
     je END_PROGRAM 
 
     // scanf("%d", &query_code);
@@ -250,16 +339,16 @@ main:
     // 3 - DELETE
     // 4 - DEFRAGMENTATION
 
-    cmp $1, query_code
+    cmpl $1, query_code
     je HANDLE_ADD
 
-    cmp $2, query_code
+    cmpl $2, query_code
     je HANDLE_GET
 
-    cmp $3, query_code
+    cmpl $3, query_code
     je HANDLE_DELETE
 
-    cmp $4, query_code
+    cmpl $4, query_code
     je HANDLE_DEFRAGMENTATION
 
     HANDLE_ADD:
@@ -271,7 +360,7 @@ main:
     popl %ebx
 
     ADD_LOOP:
-        cmp $0, file_count
+        cmpl $0, file_count
         je EXIT_ADD_LOOP
 
         // scanf("%d", &file_descriptor);
@@ -291,7 +380,7 @@ main:
         // EXEC_ADD(file_descriptor, file_size)
         call EXEC_ADD
 
-        dec file_count
+        decl file_count
         jmp ADD_LOOP
 
     EXIT_ADD_LOOP:
@@ -309,15 +398,24 @@ main:
     jmp CONTINUE_QUERY_LOOP
 
     HANDLE_DELETE:
+    pushl $file_descriptor
+    pushl $scanf_fmt
+    call scanf
+    popl %ebx
+    popl %ebx
+
+    call EXEC_DELETE
+    call PRINT_MEMORY
 
     jmp CONTINUE_QUERY_LOOP
 
     HANDLE_DEFRAGMENTATION:
-
+    call EXEC_DEFRAGMENTATION
+    call PRINT_MEMORY
     jmp CONTINUE_QUERY_LOOP
 
     CONTINUE_QUERY_LOOP:
-    dec query_count
+    decl query_count
     jmp QUERY_LOOP
 
     END_PROGRAM:
